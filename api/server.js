46eb8d71fcec67e544554da41fe55a1777c13b0c
Oluwajoba <oluwajobabello@gmail.com>
2019-11-14 23:26:30 +0100

    update server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
 
-const authRouter = require('../auth/auth-router.js');
+const authRouter = require('../node-server-testing-challenge/auth/auth-router.js.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
