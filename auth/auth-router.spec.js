defc14727301e08d75fd61b484bcfbecd6bac0df
Oluwajoba <oluwajobabello@gmail.com>
2019-11-16 23:13:13 +0100

    code clean-up / complete MVP

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -3,18 +3,18 @@ const server = require("../api/server")
 
 
 describe("router", () => {
-    describe("[GET] / endpoint", () => {
-        test("test db env", () => {
-            expect(process.env.DB_ENV).toBe("testing")
-        })
-
-        test("should return status code 200 ok", async () => {
-            const response = await request(server).post("/api/auth/register")
-            .send({username: "test", password: "1234"})
-            expect(response.status).toBe(201)
-            return request(server).get("/")
-            .expect("content-length", "11")
-            .expect("content-type", /html/)
-        })
+  describe("[GET] / endpoint", () => {
+    test("test db env", () => {
+      expect(process.env.DB_ENV).toBe("testing")
     })
+
+    test("should return status code 200 ok", async () => {
+      const response = await request(server).post("/api/auth/register")
+        .send({ username: "test", password: "1234" })
+      expect(response.status).toBe(201)
+      return request(server).get("/")
+        .expect("content-length", "11")
+        .expect("content-type", /html/)
+    })
+  })
 })
--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -2,16 +2,16 @@ const users = require("./users-model")
 const db = require("../database/dbConfig")
 
 beforeEach(async () => {
-    await db('users').truncate();
+  await db('users').truncate();
 });
 
 describe("users model", () => {
-    describe("insert function", () => {
-        test("should insert a user", async () => {
-            await users.add({ username: "tessy", password: "1234" })
+  describe("insert function", () => {
+    test("should insert a user", async () => {
+      await users.add({ username: "tessy", password: "1234" })
 
-            const Users = await db("users")
-            expect(Users).toHaveLength(1)
-        })
+      const Users = await db("users")
+      expect(Users).toHaveLength(1)
     })
+  })
 })
